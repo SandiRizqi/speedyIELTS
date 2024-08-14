87014fdf306fb95ac480483669627eb66f58e250
SANDI <sandi@SANDIs-MacBook-Pro.local>
2024-08-14 23:56:24 +0700

    change response writing

--- src/app/dashboard/writing/writing-full/_writingone/FeedBack.jsx
+++ src/app/dashboard/writing/writing-full/_writingone/FeedBack.jsx
@@ -12,28 +12,28 @@ export default function Feedback({feedback, loading}) {
             <div className="flow-root">
                 <dl className="-my-3 divide-y divide-gray-100 text-sm">
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium text-gray-900">Grammar & Accuracy</dt>
-                        <dd className="text-gray-700 sm:col-span-2">{feedback.grammar}</dd>
+                        <dt className="font-medium text-gray-900 dark:text-slate-400">Grammar & Accuracy</dt>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.grammar?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium text-gray-900">Lexical Resource</dt>
-                        <dd className="text-gray-700 sm:col-span-2">{feedback.lexical}</dd>
+                        <dt className="font-medium text-gray-900 dark:text-slate-400">Lexical Resource</dt>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.lexical?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium text-gray-900">Vocabulary Range</dt>
-                        <dd className="text-gray-700 sm:col-span-2">{feedback.vocabulary}</dd>
+                        <dt className="font-medium text-gray-900 dark:text-slate-400">Vocabulary Range</dt>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.vocabulary?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium text-gray-900">Coherence & Cohetion</dt>
-                        <dd className="text-gray-700 sm:col-span-2">{feedback.cohen}</dd>
+                        <dt className="font-medium text-gray-900 dark:text-slate-400">Coherence & Cohesion</dt>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.cohen?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-                        <dt className="font-medium text-gray-900">Task Achivement</dt>
-                        <dd className="text-gray-700 sm:col-span-2">{feedback.task}</dd>
+                        <dt className="font-medium text-gray-900 dark:text-slate-400">Task Achivement</dt>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.task?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
--- src/app/dashboard/writing/writing-one/FeedBack.jsx
+++ src/app/dashboard/writing/writing-one/FeedBack.jsx
@@ -13,27 +13,27 @@ export default function Feedback({feedback, loading}) {
                 <dl className="-my-3 divide-y divide-gray-100 text-sm">
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
                         <dt className="font-medium text-gray-900 dark:text-slate-400">Grammar & Accuracy</dt>
-                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.grammar}</dd>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.grammar?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
                         <dt className="font-medium text-gray-900 dark:text-slate-400">Lexical Resource</dt>
-                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.lexical}</dd>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.lexical?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
                         <dt className="font-medium text-gray-900 dark:text-slate-400">Vocabulary Range</dt>
-                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.vocabulary}</dd>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.vocabulary?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
                         <dt className="font-medium text-gray-900 dark:text-slate-400">Coherence & Cohesion</dt>
-                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.cohen}</dd>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.cohen?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
                         <dt className="font-medium text-gray-900 dark:text-slate-400">Task Achivement</dt>
-                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.task}</dd>
+                        <dd className="text-gray-700 sm:col-span-2 dark:text-slate-400">{feedback.task?.score}</dd>
                     </div>
 
                     <div className="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
