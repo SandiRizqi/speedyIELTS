d552baecdf5cf9d2eff12eb2fef0c93301d53c13
SANDI <sandi@SANDIs-MacBook-Pro.local>
2024-08-16 16:14:34 +0700

    add exe deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check out to the main branch
+git checkout main
+
+# Merge changes from the develop branch into main
+git merge develop
+
+# Push the merged changes to the origin main branch
+git push origin main
+
+# Check out back to the develop branch
+git checkout develop
