69acbfd227395b413c45562be45d100df8d69a07
SANDI <sandi@SANDIs-MacBook-Pro.local>
2024-09-27 16:19:19 +0700

    add exercises page

---
+++ src/app/dashboard/exercises/page.tsx
@@ -0,0 +1,20 @@
+import MiniPractice from "./MiniPractice";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+
+export const metadata: Metadata = {
+  title: "Speedy IELTS | Exercices",
+  description:
+    "",
+};
+
+const Page = () => {
+  return (
+    <DefaultLayout>
+      <MiniPractice />
+    </DefaultLayout>
+  );
+};
+
+export default Page;
