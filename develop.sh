37109e5bf3d6274094b43ce8a6fb1d070490a42a
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-05-20 10:41:27 +0700

    add appcheck

--- develop.sh
+++ develop.sh
@@ -4,7 +4,7 @@
 git add .
 
 # Merge changes from the develop branch into main
-git commit -m "fix quota check"
+git commit -m "add appcheck"
 
 # Push the merged changes to the origin main branch
 git push origin develop
